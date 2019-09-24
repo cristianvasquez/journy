2c4b25ffaf725817503360bb5669d25680b18f95
Cristian Vasquez <cristianvasquez@gmail.com>
2019-09-24 15:48:57 +0200

    Added markdown

---
+++ newTab/newTab.js
@@ -0,0 +1,6 @@
+// start in view mode
+textarea.style.display = "none";
+changeModeButton.innerText = "VIEW";
+changeModeButton.style.background = "#fb9f89";
+
+darkModeButton.addEventListener("click", toggleDarkMode);
---
+++ notepad/notepad.js
@@ -0,0 +1,4 @@
+// start in edit mode
+markdownTarget.style.display = "none";
+changeModeButton.innerText = "EDIT";
+changeModeButton.style.background = "#81ae9d";
